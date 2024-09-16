7cf8c717a18062fcfed9011bea54a886f9ca4e99
tushar-kamra <tushar.kamra25@gmail.com>
2024-09-16 22:49:13 +0530

    intro notes added

--- app.js
+++ app.js
@@ -4,5 +4,14 @@
 
 const { calculateMultiply, calculateSum } = require("./calculate");
 
-calculateMultiply(3, 4);
-calculateSum(3, 4);
+// calculateMultiply(3, 4);
+// calculateSum(3, 4);
+
+// console.log(require("./data.json"));
+
+require("./test");
+// require("./test2");
+
+console.log(__filename, __dirname);
+
+// console.log(globalThis);
